53add8dad7d908d0ebea2d122e74608a9a4c4c60
Hrivu <Cool>
2024-04-22 16:46:39 -0400

    modified:   main.py

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ fps = 60
 
 playerx = 400
 playery = 400
-accelerationy = -0.5
+accelerationy = 0.5
 velocityy = 0
 accelerationx = 0
 velocityx = 0
+touch = False
+
 
 class platform:
     def __init__(self, x, y, width, height):
@@ -33,32 +35,40 @@ class platform:
     def get_coords(self):
         return (self.x, self.y)
 
-platforms.append(platform(0, 500, 800, 100))
+platforms.append(platform(200, 500, 400, 40))
 
 def player_control():
-    global velocityx, playerx, accelerationx, velocityy, playery, accelerationy
+    global velocityx, playerx, accelerationx, velocityy, playery, accelerationy,touch
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a]:
         accelerationx = -0.15
     if keys[pygame.K_d]:
         accelerationx = 0.15
+    if keys[pygame.K_SPACE]:
+        if touch:
+            velocityy = -10
+    
     #Moving Control on velocity
     velocityx = velocityx + accelerationx
     playerx = playerx + velocityx
-    accelerationx -= -0.05
-    if pygame.key.get_pressed() == []:
-        accelerationx = 0
+    accelerationx -= 0.05
+    event_list = pygame.event.get(pygame.KEYDOWN)
+    touch = False
+    if len(event_list) == 0:
+        if(accelerationx < 0):
+            accelerationx = 0
 
     #Gravity and collision!
     velocityy += accelerationy
     playery += velocityy
     # Collision detection between player and platforms
     for plat in platforms:
-        if plat.get_rect().colliderect(pygame.Rect(playerx, playery, 100, 100)):
+        if plat.get_rect().colliderect(pygame.Rect(playerx, playery, 50, 20)):
             # Collision detected, handle it here
             # For example, stop player's vertical movement and adjust player's position
             velocityy = 0
-            playery = plat.y - 100
+            playery = plat.y - 20
+            touch = True
     
 
 while True:
@@ -70,6 +80,6 @@ while True:
     for plat in platforms:
         plat.draw(screen)
     player_control()
-    pygame.draw.circle(screen, (255, 0, 0), (playerx, playery), 50)
+    pygame.draw.circle(screen, (255, 0, 0), (playerx, playery), 20)
     pygame.display.flip()
     pygame.time.Clock().tick(fps)
